60c420752ad83c559dad1ddfbd9f9113909d2e72
Josh Edson <joshedson@gmail.com>
2024-08-16 15:38:45 -0700

    Initial Commit

---
+++ SwiftUI-USGS-EarthQuakes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-USGS-EarthQuakes
+//
+//  Created by Josh Edson on 8/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-USGS-EarthQuakes/SwiftUI_USGS_EarthQuakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_USGS_EarthQuakesApp.swift
+//  SwiftUI-USGS-EarthQuakes
+//
+//  Created by Josh Edson on 8/16/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_USGS_EarthQuakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
